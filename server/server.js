86d75ae2577a6428517a01665f3437eb1d40cd8a
Guillaume Renouvin <guillaumer@theodo.fr>
2017-02-20 22:14:46 +0100

    branch1 init

--- server/server.js
+++ server/server.js
@@ -10,28 +10,10 @@ var fs = require('fs');
 var https = require('https');
 var request = bluebird.promisifyAll(require('request'), {multiArgs: true});
 var _ = require('lodash');
-var gpio = require('rpi-gpio');
 
 // VARIABLE
 var STATUS = require('./constants/status');
 var URLS = require('./constants/urls');
-var gpiosLedArray = [
-  {red: 18, green: 23},
-  {red: 24, green: 25},
-  {red: 12, green: 16},
-];
-var gpioBip = 7;
-
-// INIT GPIO
-gpio.setMode(gpio.MODE_BCM);
-_.each(gpiosLedArray, function(gpiosLed) {
-  gpio.setup(gpiosLed.red, gpio.DIR_OUT, function() {
-    gpio.write(gpiosLed.red, true);
-  });
-  gpio.setup(gpiosLed.green, gpio.DIR_OUT, function() {
-    gpio.write(gpiosLed.green, true);
-  });
-});
 
 var optionsGithub = {
   url: URLS.GITHUB + config.user + '/' + config.repo + '/pulls?state=open',
@@ -95,83 +77,7 @@ var getCircleCiBuilds = function(optionsCircleCi) {
   });
 };
 
-// GPIO
-var setLed = function(pin, status) {
-  switch (status) {
-    case STATUS.SUCCESS:
-      gpio.write(pin.green, false);
-      gpio.write(pin.red, true);
-      break;
-    case STATUS.FAILED:
-      gpio.write(pin.green, true);
-      gpio.write(pin.red, false);
-      break;
-    case STATUS.RUNNING:
-      gpio.write(pin.green, false);
-      gpio.write(pin.red, false);
-      break;
-    case STATUS.EMPTY:
-      gpio.write(pin.green, true);
-      gpio.write(pin.red, true);
-      break;
-  }
-};
-
-var setLeds = function(statusArray) {
-  _.each(statusArray, function(status, index) {
-    if (index > gpiosLedArray.length) { return false; }
-    console.log(status.build, index);
-    switch (status.build) {
-      case STATUS.NO_TESTS:
-      case STATUS.FIXED:
-      case STATUS.SUCCESS:
-        setLed(gpiosLedArray[index], STATUS.SUCCESS);
-        break;
-      case STATUS.FAILED:
-        setLed(gpiosLedArray[index], STATUS.FAILED);
-        break;
-      case STATUS.RUNNING:
-        setLed(gpiosLedArray[index], STATUS.RUNNING);
-        break;
-    };
-  });
-
-  for (var i = statusArray.length; i < gpiosLedArray.length; i++) {
-    console.log('empty', i);
-    setLed(gpiosLedArray[i], STATUS.EMPTY);
-  }
-};
-
-var setBip = function(bool) {
-  gpio.write(gpioBip, bool);
-};
-
 // ROUTES
-var previousCommits = [];
-setInterval(function() {
-  bluebird.all([
-    getGithubPR(optionsGithub),
-    getCircleCiBuilds(optionsCircleCi),
-  ]).then(function(responses) {
-    var statusArray = _.intersectionBy(responses[1], responses[0], 'commit');
-    var newCommits = _.map(statusArray, function(status) {
-      return status.commit;
-    });
-
-    if (_.difference(newCommits, previousCommits).length !== 0) {
-      setBip(true);
-      setTimeout(function() {
-        setBip(false);
-      }, 2000);
-    }
-
-    previousCommits = newCommits;
-    setLeds(statusArray);
-  });
-},
-  3000
-);
-
 app.get('/commits', function(req, res) {
   bluebird.all([
     getGithubPR(optionsGithub),
@@ -180,13 +86,6 @@ app.get('/commits', function(req, res) {
     var statusArray = _.intersectionBy(responses[1], responses[0], 'commit');
     res.write(JSON.stringify(statusArray));
     res.end();
-  });
-});
-
-process.on('SIGINT', function() {
-  gpio.destroy(function() {
-    console.log('All pins unexported');
-    process.exit();
   });
 });
 
